c1a4635f8cd8108191b554431dc35e3d5105f41c
Marcondys Santos Bezerra Silva <marcondysbezerra@gmail.com>
2024-01-04 17:16:40 -0300

    Refatoração da função pagina_inicial

--- projectDjango/myApp/views.py
+++ projectDjango/myApp/views.py
@@ -1,36 +1,48 @@
 import socket
+from requests import get
 from django.shortcuts import render
 from projectDjango.myApp.forms import FormPortTest
 
 
-# Create your views here.
 def pagina_inicial(request):
+    result = ''
+    erro = ''
 
-    # if this is a POST request we need to process the form data
     if request.method == "POST":
-        # create a form instance and populate it with data from the request:
         form = FormPortTest(request.POST)
-        # check whether it's valid:
         if form.is_valid():
-            # process the data in form.cleaned_data as required
             host = form.cleaned_data['ip_address_form']
             port = form.cleaned_data['port_form']
 
             connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            print(connection)
             connection.settimeout(1)
-            result = connection.connect_ex((host, port))
-            # connection.close()
+
+            try:
+                result = connection.connect_ex((host, port))
+            except socket.gaierror as e:
+                erro = e
+
+            connection.close()
 
             context = {
                 'form': form,
                 'result': result,
                 'host': host,
                 'port': port,
+                'erro': erro,
             }
-            # redirect to a new URL:
+
             return render(request, 'ipv4/index.html', context=context)
 
-    # if a GET (or any other method) we'll create a blank form
     else:
         form = FormPortTest()
     return render(request, "ipv4/index.html", {"form": form})
+
+
+def your_ip(request):
+    ip = get('https://api.ipify.org').text
+    context = {'ip': ip}
+    return render(request, 'ipv4/index.html', context=context)
+
+

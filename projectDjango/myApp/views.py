c389e28e4ed387d8dddb48140e4e99037b25578e
Marcondys Santos Bezerra Silva <marcondysbezerra@gmail.com>
2024-01-18 11:06:52 -0300

    Estilização

--- projectDjango/myApp/views.py
+++ projectDjango/myApp/views.py
@@ -1,6 +1,8 @@
 import socket
+import os
 from requests import get
-from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import render, HttpResponse
 from projectDjango.myApp.forms import FormPortTest
 
 
@@ -14,8 +16,7 @@ def pagina_inicial(request):
             host = form.cleaned_data['ip_address_form']
             port = form.cleaned_data['port_form']
 
-            connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            print(connection)
+            connection = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
             connection.settimeout(1)
 
             try:
@@ -43,6 +44,46 @@ def pagina_inicial(request):
 def your_ip(request):
     ip = get('https://api.ipify.org').text
     context = {'ip': ip}
-    return render(request, 'ipv4/index.html', context=context)
+    return render(request, 'ipv4/meuip.html', context=context)
 
 
+def teste_ipv4(request):
+    result = ''
+    erro = ''
+
+    if request.method == "POST":
+        form = FormPortTest(request.POST)
+        if form.is_valid():
+            host = form.cleaned_data['ip_address_form']
+            port = form.cleaned_data['port_form']
+
+            connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            connection.settimeout(1)
+
+            try:
+                result = connection.connect_ex((host, port))
+            except socket.gaierror as e:
+                erro = e
+
+            connection.close()
+
+            context = {
+                'form': form,
+                'result': result,
+                'host': host,
+                'port': port,
+                'erro': erro,
+            }
+
+            return render(request, 'ipv4/prot.html', context=context)
+    else:
+        form = FormPortTest()
+    return render(request, "ipv4/prot.html", {"form": form})
+
+"""def robots(request):
+    if not settings.DEBUG:
+        pass
+    else:
+        path = os.path.join(settings.BASE_DIR, 'templates/static/robots.txt')
+    with open(path, 'r') as arq:
+        return HttpResponse(arq)"""
